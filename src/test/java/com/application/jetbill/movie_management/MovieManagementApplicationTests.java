00ed17e8b5404f822da5d652a371945e2751c1c1
jetbill <william.renteria.m@gmail.com>
2025-01-28 15:42:00 -0500

    springboot project setup

---
+++ src/test/java/com/application/jetbill/movie_management/MovieManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.jetbill.movie_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
